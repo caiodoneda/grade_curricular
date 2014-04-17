65edd45b2c1785881eb728b979ea3e48816110dd
Antonio Carlos Mariani <a.c.mariani@inf.ufsc.br>
2014-04-17 16:58:05 -0300

    Tratando diferença entre view e configure

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ $returnurl = new moodle_url('/course/index.php', array('categoryid'=>$context->i
 $category = $DB->get_record('course_categories', array('id'=>$context->instanceid), '*', MUST_EXIST);
 
 if (optional_param('savechanges', false, PARAM_BOOL) && confirm_sesskey()) {
+    require_capability('local/grade_curricular:configure', $context);
     gc_save($contextid, $category);
     redirect($baseurl, get_string('changessaved'), 1);
 }
@@ -123,24 +124,29 @@ $type_options = array(0=>get_string('not_classified', 'local_grade_curricular'),
                       2=>get_string('optional', 'local_grade_curricular'),
                       3=>get_string('ignore', 'local_grade_curricular'));
 
+$attributes = array();
+if(! has_capability('local/grade_curricular:configure' , $context)) {
+    $attributes['disabled'] = 'disabled';
+}
+
 $data = array();
 foreach($courses as $c) {
     $tab = new html_table();
     $tab->data = array();
 
     $tab->data[] = array(get_string('type', 'local_grade_curricular'),
-                         html_writer::select($type_options, "type[$c->id]", $c->type, false));
+                         html_writer::select($type_options, "type[$c->id]", $c->type, false, $attributes));
     $tab->data[] = array(get_string('workload', 'local_grade_curricular'),
-                         html_writer::empty_tag('input', array('type'=>'text', 'name'=>"workload[$c->id]", 'value'=>$c->workload, 'size'=>5)));
+                         html_writer::empty_tag('input', array('type'=>'text', 'name'=>"workload[$c->id]", 'value'=>$c->workload, 'size'=>5) + $attributes));
 
     $startdate = get_string('from') . ': '
-                 . html_writer::select_time('days', "startdays[$c->id]", $c->inscribestartdate)
-                 . html_writer::select_time('months', "startmonths[$c->id]", $c->inscribestartdate)
-                 . html_writer::select_time('years', "startyears[$c->id]", $c->inscribestartdate);
+                 . html_writer::select_time('days', "startdays[$c->id]", $c->inscribestartdate, 0, $attributes)
+                 . html_writer::select_time('months', "startmonths[$c->id]", $c->inscribestartdate, 0, $attributes)
+                 . html_writer::select_time('years', "startyears[$c->id]", $c->inscribestartdate, 0, $attributes);
     $enddate   = get_string('to') . ': '
-                 . html_writer::select_time('days', "enddays[$c->id]", $c->inscribeenddate)
-                 . html_writer::select_time('months', "endmonths[$c->id]", $c->inscribeenddate)
-                 . html_writer::select_time('years', "endyears[$c->id]", $c->inscribeenddate);
+                 . html_writer::select_time('days', "enddays[$c->id]", $c->inscribeenddate, 0, $attributes)
+                 . html_writer::select_time('months', "endmonths[$c->id]", $c->inscribeenddate, 0, $attributes)
+                 . html_writer::select_time('years', "endyears[$c->id]", $c->inscribeenddate, 0, $attributes);
     $tab->data[] = array(get_string('inscribeperiodo', 'local_grade_curricular'),
                    $startdate . '   ' . $enddate);
 
@@ -152,7 +158,7 @@ foreach($courses as $c) {
     }
 
     $tab->data[] = array(get_string('dependency', 'local_grade_curricular'),
-                         html_writer::select($course_options, "dependencies[$c->id]", $c->coursedependencyid, false));
+                         html_writer::select($course_options, "dependencies[$c->id]", $c->coursedependencyid, false, $attributes));
 
     $curl = new moodle_url('/course/view.php', array('id'=>$c->id));
     $data[] = array(html_writer::link($curl, format_string($c->fullname), array('target'=>'_new')),
@@ -174,16 +180,16 @@ for($i=0; $i <= count($courses); $i++) {
 
 echo html_writer::start_tag('div');
 echo html_writer::tag('B', get_string('minoptionalcourses', 'local_grade_curricular'));
-echo html_writer::select($options_opt, "minoptionalcourses", $grade->minoptionalcourses, false);
+echo html_writer::select($options_opt, "minoptionalcourses", $grade->minoptionalcourses, false, $attributes);
 
 echo html_writer::empty_tag('br');
 echo html_writer::tag('B', get_string('maxoptionalcourses', 'local_grade_curricular'));
-echo html_writer::select($options_opt, "maxoptionalcourses", $grade->maxoptionalcourses, false);
+echo html_writer::select($options_opt, "maxoptionalcourses", $grade->maxoptionalcourses, false, $attributes);
 
 echo html_writer::empty_tag('br');
 $yesno_options = array('1'=>get_string('yes'), '0'=>get_string('no'));
 echo html_writer::tag('B', get_string('optionalatonetime', 'local_grade_curricular'));
-echo html_writer::select($yesno_options, "optionalatonetime", $grade->optionalatonetime, false);
+echo html_writer::select($yesno_options, "optionalatonetime", $grade->optionalatonetime, false, $attributes);
 
 $plugins = core_component::get_plugin_list('local');
 if(isset($plugins['inscricoes'])) {
@@ -191,7 +197,7 @@ if(isset($plugins['inscricoes'])) {
     $editions_opt += gc_get_potential_editions($context, $grade->id);
     echo html_writer::empty_tag('br');
     echo html_writer::tag('B', get_string('edition', 'local_grade_curricular'));
-    echo html_writer::select($editions_opt, "inscricoeseditionid", $grade->inscricoeseditionid, false);
+    echo html_writer::select($editions_opt, "inscricoeseditionid", $grade->inscricoeseditionid, false, $attributes);
 } else {
     echo html_writer::empty_tag('input', array('type'=>'hidden', 'name'=>'inscricoeseditionid', 'value'=>0));
 }
@@ -200,7 +206,7 @@ $cohorts_opt = gc_get_cohorts($context);
 $cohorts_opt[0] = get_string('no_cohort', 'local_grade_curricular');
 echo html_writer::empty_tag('br');
 echo html_writer::tag('B', get_string('studentcohort', 'local_grade_curricular'));
-echo html_writer::select($cohorts_opt, "studentcohortid", $grade->studentcohortid, false);
+echo html_writer::select($cohorts_opt, "studentcohortid", $grade->studentcohortid, false, $attributes);
 
 $courses_opt = array();
 $courses_opt[0] = get_string('no_notecourse', 'local_grade_curricular');
@@ -209,7 +215,7 @@ foreach($courses as $c) {
 }
 echo html_writer::empty_tag('br');
 echo html_writer::tag('B', get_string('notecourse', 'local_grade_curricular'));
-echo html_writer::select($courses_opt, "notecourseid", $grade->notecourseid, false);
+echo html_writer::select($courses_opt, "notecourseid", $grade->notecourseid, false, $attributes);
 
 $all_roles = role_get_names();
 $ctx_roles = get_roles_for_contextlevels(CONTEXT_COURSE);
@@ -219,13 +225,13 @@ foreach($ctx_roles AS $id=>$roleid) {
 }
 echo html_writer::empty_tag('br');
 echo html_writer::tag('B', get_string('tutorrole', 'local_grade_curricular'));
-echo html_writer::select($roles_opt, "tutorroleid", $grade->tutorroleid, false);
+echo html_writer::select($roles_opt, "tutorroleid", $grade->tutorroleid, false, $attributes);
 
 echo html_writer::end_tag('div');
 
 echo html_writer::empty_tag('br');
 echo html_writer::start_tag('div', array('class'=>'buttons'));
-echo html_writer::empty_tag('input', array('type'=>'submit', 'name'=>'savechanges', 'value'=>get_string('savechanges')));
+echo html_writer::empty_tag('input', array('type'=>'submit', 'name'=>'savechanges', 'value'=>get_string('savechanges')) + $attributes);
 echo html_writer::link($returnurl, '  ' . get_string('cancel'));
 echo html_writer::end_tag('div');
 
