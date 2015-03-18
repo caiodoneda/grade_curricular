bb823bd5d8079df4a7ed16a0b713abe67d3a22ab
caiodoneda <caiodoneda@gmail.com>
2015-03-18 14:33:46 -0300

    alterando nome de campo editionid -> activityid

--- js/gradecurricular.js
+++ js/gradecurricular.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
-	var options = $('#menuinscricoeseditionid').find('option');
+	var options = $('#menuinscricoesactivityid').find('option');
 	options.each(function(k, v) {
 		if (v.value != 0 && v.selected) {
 			$('#menustudentcohortid').prop('disabled', true);
 		}
 	});
 
-	$('#menuinscricoeseditionid').change(function(e){
-		if (e.target.value == 0) {
+	$('#menuinscricoesactivityid').change(function(e){
+        if (e.target.value == 0) {
 			$('#menustudentcohortid').prop('disabled', false);
 		} else {
 			$('#menustudentcohortid').prop('disabled', true);
@@ -17,15 +17,15 @@ $(document).ready(function(){
 	var options = $('#menustudentcohortid').find('option');
 	options.each(function(k, v) {
 		if (v.value != 0 && v.selected) {
-			$('#menuinscricoeseditionid').prop('disabled', true);
+			$('#menuinscricoesactivityid').prop('disabled', true);
 		}
 	});
 
 	$('#menustudentcohortid').change(function(e){
 		if (e.target.value == 0) {
-			$('#menuinscricoeseditionid').prop('disabled', false);
+			$('#menuinscricoesactivityid').prop('disabled', false);
 		} else {
-			$('#menuinscricoeseditionid').prop('disabled', true);
+			$('#menuinscricoesactivityid').prop('disabled', true);
 		}
 	})
 });
