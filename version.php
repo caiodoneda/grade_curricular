abc5b1bb34532eb0563365ba0a2ad2babfbb6092
caiodoneda <caiodoneda@gmail.com>
2015-08-28 13:21:04 -0300

    atualizando versão

--- version.php
+++ version.php
@@ -2,5 +2,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_grade_curricular';
-$plugin->version = 2015022500;
+$plugin->version = 2015082800;
 $plugin->requires  = 2013111800; //Moodle 2.6.1
