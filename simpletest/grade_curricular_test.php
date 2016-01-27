e7a12021f230fd4e1b1759034c9868ca0002ba17
Caio Doneda <caiodoneda@gmail.com>
2016-01-27 14:17:53 -0200

    testando função que busca cursos da grade curricular

--- simpletest/grade_curricular_test.php
+++ simpletest/grade_curricular_test.php
@@ -33,51 +33,50 @@ class grade_curricular_test extends advanced_testcase {
 
     	$this->category = $this->getDataGenerator()->create_category();
     	$this->completions_info = array();
-
-        $this->create_and_enroll_students_at_courses();
-        $this->create_criterias_at_courses();
-        $this->complete_courses();
                 
         $this->grade_curricular = $this->create_fake_grade_curricular();
-        $this->grade_curricular_courses = $this->associate_courses_to_grade_curricular(); 
     }
 
-    protected function create_and_enroll_students_at_courses() {
-        global $DB;
-
-        for ($i = 1; $i <= 10; $i++) {
-            $this->students[$i] = $this->getDataGenerator()->create_user();
+    protected function create_courses($amount) {
+        for ($i = 1; $i <= $amount; $i++) {
             $this->courses[$i] = $this->getDataGenerator()->create_course(array('category' => $this->category->id));
-            $this->completions_info[$this->courses[$i]->id] =  new completion_info($this->courses[$i]);
-            $this->courses[$i]->enablecompletion = 1;
-            $DB->update_record('course', $this->courses[$i]);
         }
+    }
 
-        foreach ($this->students as $key => $student) {
-            foreach ($this->courses as $key => $course) {
-                $this->getDataGenerator()->enrol_user($student->id, $course->id, 5);
+    protected function create_students($amount) {
+        for ($i = 1; $i <= $amount; $i++) {
+            $this->students[$i] = $this->getDataGenerator()->create_user();
+        }
+    }
+
+    protected function enrol_students($students = array(), $courses = array()) {
+        foreach ($courses as $key => $course) {
+            foreach ($students as $key => $student) {
+                $this->getDataGenerator()->enrol_user($student->id, $course->id, 5); //5 is the student roleid.
             }
         }
     }
 
-    
-    protected function create_criterias_at_courses() {
-        foreach ($this->courses as $course) {
-            $completion = $this->completions_info[$course->id];
+    protected function create_courses_completions($courses) {
+        foreach ($courses as $course) {
+            //Creating and enabling new completion for each course.
+            $this->completions_info[$course->id] = new completion_info($course);
+            $course->enablecompletion = 1;
+            $DB->update_record('course', $course);
+
             $data = new stdClass();
             $data->criteria_self = 1; //Just setting any value to not be empty. The class completion_criteria will vaerify this value.
-            $data->id = $completion->course_id;
+            $data->id = $course->id;
             $criterion = new completion_criteria_self();
             $criterion->update_config($data);
-            $this->completions_info[$course->id]->criterion = $criterion;
+            $this->completions_info[$course->id]->criterion = $criterion;//maybe we don't need this.
         }
     }
 
-    protected function complete_courses() {     
-        foreach ($this->courses as $course) {
-            $completion = new completion_info($course);
-            foreach ($this->students as $student) {
-                $student_completion = $completion->get_completion($student->id, 1); //1 = COMPLETION_CRITERIA_TYPE_SELF
+    protected function complete_courses($courses = array(), $students = array()) {     
+        foreach ($courses as $course) {
+            foreach ($students as $student) {
+                $student_completion = $this->completions_info[$course->id]->get_completion($student->id, 1); //1 = COMPLETION_CRITERIA_TYPE_SELF
                 $student_completion->mark_complete();
             }
         }
@@ -104,7 +103,7 @@ class grade_curricular_test extends advanced_testcase {
         return $DB->get_record('grade_curricular', array('id'=>$gradeid));
     }
 
-    protected function associate_courses_to_grade_curricular() {
+    protected function associate_courses_to_grade_curricular($courses, $optative_amount, $mandatory_amount) {
         global $DB;
 
         $record = new stdClass();
@@ -113,57 +112,60 @@ class grade_curricular_test extends advanced_testcase {
         $record->inscribeenddate = time();
         $record->coursedependencyid = 0;
         $record->timemodified = time();
-
-        //The conditions are used to generate mandatory and optative courses, also we set differente workloads. 
-        // ps: courses with workload = 0 are not sent to certificate system.
-        foreach ($this->courses as $key => $course) {
-            $record->courseid = $course->id;
-            if ($key < 5) {
-                $record->type = 1;
-            } else {
-                $record->type = 2;
-            }
-
-            if ($key % 2 == 0) {
-                $record->workload = 1;
-            } else {
-                $record->workload = 0;
-            }
-
+        
+        for ($i = 1; $i <= $optative_amount; $i++) {
+            $record->courseid = $courses[$i]->id;
+            $record->type = 2;
+            $record->workload = 1;
             $DB->insert_record('grade_curricular_courses', $record);
         }
 
-        return $DB->get_records('grade_curricular_courses', array('gradecurricularid'=>$this->grade_curricular->id));
-    }
-
-    public function test_is_grade_curricular_created() {
-        global $DB;
-
-        $this->resetAfterTest(true);
-        $this->assertTrue($DB->record_exists('grade_curricular', array('id'=>$this->grade_curricular->id)));    
-    }
-
-    public function test_courses_are_associated() {
-        global $DB;
-
-        $this->resetAfterTest(true);
-        foreach ($this->courses as $key => $course) {
-            $this->assertTrue($DB->record_exists('grade_curricular_courses', 
-                              array('gradecurricularid'=>$this->grade_curricular->id, 
-                                    'courseid'=>$course->id)));     
+        for ($i = 1; $i <= $mandatory_amount; $i++) {
+            $record->courseid = $courses[$i]->id;
+            $record->type = 1;
+            $record->workload = 1;
+            $DB->insert_record('grade_curricular_courses', $record);
         }
+
+        $this->grade_curricular_courses = $DB->get_records('grade_curricular_courses', array('gradecurricularid'=>$this->grade_curricular->id));
     }
 
-    public function test_check_for_completions() {
+    public function test_get_courses() {
         $this->resetAfterTest(true);
-
-        foreach ($this->courses as $key => $course) {
-            $completion = new completion_info($course);
-            foreach ($this->students as $key => $student) {
-                $student_completion = $completion->get_completion($student->id, 1); //1 = COMPLETION_CRITERIA_TYPE_SELF
-                $this->assertTrue($student_completion->is_complete());      
-            }
-        }
+        $this->create_courses(10);
+        $this->associate_courses_to_grade_curricular($this->courses, 5, 5);
+        $this->assertEquals(10, count($this->grade_curricular_courses));
     }
+
+    // public function test_is_grade_curricular_created() {
+    //     global $DB;
+
+    //     $this->resetAfterTest(true);
+    //     $this->assertTrue($DB->record_exists('grade_curricular', array('id'=>$this->grade_curricular->id)));    
+    // }
+
+    // public function test_courses_are_associated() {
+    //     global $DB;
+
+    //     $this->resetAfterTest(true);
+    //     foreach ($this->courses as $key => $course) {
+    //         $this->assertTrue($DB->record_exists('grade_curricular_courses', 
+    //                           array('gradecurricularid'=>$this->grade_curricular->id, 
+    //                                 'courseid'=>$course->id)));     
+    //     }
+    // }
+
+    // public function test_check_for_completions() {
+    //     $this->resetAfterTest(true);
+
+    //     foreach ($this->courses as $key => $course) {
+    //         $completion = new completion_info($course);
+    //         foreach ($this->students as $key => $student) {
+    //             $student_completion = $completion->get_completion($student->id, 1); //1 = COMPLETION_CRITERIA_TYPE_SELF
+    //             $this->assertTrue($student_completion->is_complete());      
+    //         }
+    //     }
+    // }
+
 
 }
